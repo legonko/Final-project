fix detection, add velocity calculation

lib/utils/mapping.py
@@ -13,15 +13,15 @@ def find_homography():
     # for single lane
     # for realsense
 
-    x1, y1 = 202, 480
-    x2, y2 = 265, 360
-    x3, y3 = 360, 360
-    x4, y4 = 470, 480
+    # x1, y1 = 202, 480
+    # x2, y2 = 265, 360
+    # x3, y3 = 360, 360
+    # x4, y4 = 470, 480
 
-    u1, v1 = x1, y1
-    u2, v2 = 202, 0
-    u3, v3 = 470, 0
-    u4, v4 = x4, y4
+    # u1, v1 = x1, y1
+    # u2, v2 = 202, 0
+    # u3, v3 = 470, 0
+    # u4, v4 = x4, y4
 
     # for cv camera
     # x1, y1 = 210, 480
@@ -46,6 +46,17 @@ def find_homography():
     u3, v3 = 640, 0
     u4, v4 = x4, y4'''
     
+    # for 19 cm between jr and road
+    x1, y1 = 290, 480
+    x2, y2 = 300, 380
+    x3, y3 = 360, 380
+    x4, y4 = 370, 480
+
+    u1, v1 = x1, y1
+    u2, v2 = 275, 0
+    u3, v3 = 370, 0
+    u4, v4 = x4, y4
+    
     # camera points
     points_camera = np.array([[x1, y1], [x2, y2], [x3, y3], [x4, y4]], dtype=np.float32)
     # birds-eye view points
@@ -56,11 +67,32 @@ def find_homography():
     return homography_matrix
 
 
+def find_inverse_homography():
+    x1, y1 = 290, 480
+    x2, y2 = 300, 380
+    x3, y3 = 360, 380
+    x4, y4 = 370, 480
+
+    u1, v1 = x1, y1
+    u2, v2 = 275, 0
+    u3, v3 = 370, 0
+    u4, v4 = x4, y4
+    
+    # camera points
+    points_camera = np.array([[x1, y1], [x2, y2], [x3, y3], [x4, y4]], dtype=np.float32)
+    # birds-eye view points
+    points_birdseye = np.array([[u1+200, v1+200], [u2+200, v2+200], [u3+200, v3+200], [u4+200, v4+200]], dtype=np.float32)
+
+    homography_matrix, _ = cv2.findHomography(points_birdseye, points_camera)
+    
+    return homography_matrix
+
+
 def ipm_ll(image, homography_matrix):
     image = np.asanyarray(image, dtype=np.uint8)
     image = cv2.resize(image, dsize=(640, 480))
     # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    transformed_image = cv2.warpPerspective(image, homography_matrix, (640+400, 480+200)) # cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(0,0,0))
+    transformed_image = cv2.warpPerspective(image, homography_matrix, (640+200+200, 480+200)) # cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(0,0,0))
 
     return transformed_image
 
@@ -74,7 +106,7 @@ def lanes2map(transformed_image):
 
     histogram = np.sum(transformed_image, axis=0)
  
-    peaks, _ = find_peaks(histogram,  height=4000)  # prominence=10000, prominence - min height above surrounding
+    peaks, _ = find_peaks(histogram,  height=5000)  # prominence=10000, prominence - min height above surrounding
     lanes_map = np.zeros_like(transformed_image)
     # lanes_map = cv2.cvtColor(lanes_map, cv2.COLOR_RGB2GRAY)
     peaks = np.sort(peaks)
@@ -84,16 +116,17 @@ def lanes2map(transformed_image):
             lanes_map[:, peak] = 255
         # lanes_map[:, peaks[0]] = 100 # left
         # lanes_map[:, peaks[1]] = 250 # right
-    print(peaks)
+    # print('peaks', peaks)
     
     return lanes_map, peaks
 
 
-def lane_centering(peaks, pos):
+def lane_centering(peaks):
     # if pos = (y, x)
     # to do:
     # pos == const == (0, w//2)
     # position == camera position, pos[1] = 640//2
+    pos = config.pos
     if len(peaks) == 2: # add supporting for multiple lanes
         rl = abs(pos[1] - peaks[0])
         rr = abs(peaks[1] - pos[1])
@@ -131,17 +164,17 @@ def vehicles2map(bounding_boxes, lanes_map):
         r0 = int(bbox[1])
         c1 = int(bbox[2])
         r1 = int(bbox[3])
-        # r0 = r1
-        # print(bbox)
-        # l      w         h 
-        # 4644 x 1778 x 1482
-        # k = l/w = 2.612
+        '''
+        r0 = r1
+        l       w       h 
+        4644 x 1778 x 1482
+        '''
         k = 1.2 #2.612
         w = c1 - c0
         y = int(r0 - k * w) #if int(r0 - k * w) >= 0 else 0
 
         vehicles_map[y:r0, c0:c1] = 255
-        print(r0, c0, r1, c1)
+        # print(r0, c0, r1, c1)
         # original bbox
         # c11, c22 = (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])) 
         # vehicles_map = cv2.cvtColor(vehicles_map, cv2.COLOR_GRAY2BGR)
@@ -152,21 +185,43 @@ def vehicles2map(bounding_boxes, lanes_map):
 
 def scaling(depth_img, bbox_center):
     # m
-    horizontal_line_center = (1040 // 2, bbox_center[1]) # (x, y)
-    h = 0.15 # height of camera 
-    d = get_distance(depth_img, config.depth_scale, bbox_center) # to center of bbox
-    L = get_distance(depth_img, config.depth_scale, horizontal_line_center) # to center of horizontal line from bottom bbox line
+    # bbox_center[0] = bbox_center[0]-400
+    # bbox_center[1] = bbox_center[1]-200
+    horizontal_line_center = np.array([(640+400) // 2, bbox_center[1]]) # (x, y)
+    # print('horizont: ', horizontal_line_center)
+    h = 0.13 # height of camera 
+    d = get_distance(depth_img, bbox_center) # to center of bbox
+    L = get_distance(depth_img, horizontal_line_center) # to center of horizontal line from bottom bbox line
+    # cv2.imwrite('depth_tst.jpg', depth_img)
+    # print('bbox', bbox_center)
+    # print('d: ', d, 'L: ', L)
     l = math.sqrt(d ** 2 - h ** 2) # from ground to center of bbox
+    # print('l: ', l)
     c = math.sqrt(L ** 2 - h ** 2) # from ground to center line
+    # print('c: ', c)
     alpha = math.acos(c / l) # between bbox and vertial line
-
+    # alpha is in radians !!!
     return l, alpha
 
 def get_distance(depth_img, point):
     # m
-    distance = depth_img[point[1]-15:point[1], point[0]-20:point[0]+20] # x, y
+    # points = point.reshape(-1, 2)
+    points = copy.copy(np.array(point))
+    points = points.reshape(1, 1, 2)
+    # print('_points', points, 'shape', points.shape)
+    points = ipm_pts(points, find_inverse_homography())
+    points = points.reshape(-1, 2).flatten()
+    points = np.array(points)
+    # print('points', points, 'shape', points.shape)
+    # distance = depth_img[int(point[1]-10):int(point[1]), int(point[0]-5):int(point[0]+5)] # x, y
+    distance = depth_img[int(points[1]), int(points[0])]
+    if distance == 0.0:
+        # distance = depth_img[int(point[1]-10):int(point[1]), int(point[0]-5):int(point[0]+5)]
+        # distance,_,_,_ = cv2.mean(distance)
+        distance = depth_img[int(points[1]), int(points[0]-10)]
+
     distance = distance * config.depth_scale
-    distance,_,_,_ = cv2.mean(distance)
+    # distance,_,_,_ = cv2.mean(distance)
     
     return distance
 
@@ -174,25 +229,37 @@ def get_distance(depth_img, point):
 def calculate_real_distance(depth_img, bboxes_centers):
     vectors = [] # may be dict: {bbox_center: [l, alpha]...}
     for bbox_center in bboxes_centers:
-        l, alpha =  scaling(depth_img, config.depth_scale, bbox_center)
+        l, alpha =  scaling(depth_img, bbox_center)
         vectors.append([l, alpha])
 
     return vectors
 
 
-def tracking(new_bboxes, old_bboxes):
+def tracking(new_bboxes, old_bboxes, depth_img):
     # [[x,y]] - center of w coords
     # {(x,y): [(x,y), cost]}
+    # print('__nb: ', new_bboxes)
+    # print('__ob: ', old_bboxes)
     if old_bboxes is not None:
-        new_bboxes[:, 0] = abs(new_bboxes[:, 2] - new_bboxes[:, 0]) # [[xc,yc]]
+        new_bboxes[:, 0] = new_bboxes[:, 0] + abs(new_bboxes[:, 2] - new_bboxes[:, 0]) // 2 # not true -> [[xc,yc]]
         new_bboxes = new_bboxes[:, :2]
-        old_bboxes[:, 0] = abs(old_bboxes[:, 2] - old_bboxes[:, 0])
+        old_bboxes[:, 0] = old_bboxes[:, 0] + abs(old_bboxes[:, 2] - old_bboxes[:, 0]) // 2 # true -> r, c
         old_bboxes = old_bboxes[:, :2]
         graph = graph_class.Graph()
+        print('nb: ', new_bboxes)
+        print('ob: ', old_bboxes)
 
         for vert1 in new_bboxes:
             for vert2 in old_bboxes:
-                graph.add_edge(tuple(vert1), tuple(vert2))
+                # depth_img, vert -> scaling -> l, phi
+                # l1, l2, phi1, phi2 -> calculate vector difference -> delta
+                # add_edge2
+                l1, phi1 = scaling(depth_img, copy.copy(vert1))
+                # print('l1: ', l1, 'phi1: ', phi1)
+                l2, phi2 = scaling(depth_img, copy.copy(vert2))
+                # print('l2: ', l2, 'phi2: ', phi2)
+                # graph.add_edge(tuple(vert1), tuple(vert2))
+                graph.add_edge2(vert1, vert2, l2, l1, phi2, phi1)
 
         vertices = list(graph.keys())
         new_graph = graph_class.Graph()
@@ -200,33 +267,70 @@ def tracking(new_bboxes, old_bboxes):
         for i in range(len(vertices)):
             neighbours = graph[vertices[i]] 
             min_cost, ind  = min(((item[1], index) for index, item in enumerate(neighbours)), key=lambda x: x[0])
-            new_graph.add_edge(vertices[i], neighbours[ind][0])
+            # new_graph.add_edge(vertices[i], neighbours[ind][0])
+            l1, phi1 = scaling(depth_img, copy.copy(vertices[i]))
+            l2, phi2 = scaling(depth_img, copy.copy(neighbours[ind][0]))
+            new_graph.add_edge2(vertices[i], neighbours[ind][0], l2, l1, phi2, phi1)
+        # print('new graph: ', new_graph)
 
         # dl = cost
-    return new_graph
+        return new_graph
+    else:
+        return None
 
+
+def calculate_vector_difference(l1, l2, phi1, phi2):
+    '''
+    l1 - from ground center to car on first frame (meters)
+    l2 - from ground center to car on second frame
+    phi - angle between vertical and vector l (rad)
+    '''
+    x1 = l1 * math.cos(math.pi - phi1)
+    y1 = l1 * math.sin(math.pi - phi1)
+    x2 = l2 * math.cos(math.pi - phi2)
+    y2 = l2 * math.sin(math.pi - phi2)
+    delta = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+    return delta
 
 def calculate_velocity(dt, graph):
     # calculate velocity for each vertex in graph
     vertices = list(graph.keys())
     vel_graph = copy.copy(graph)
     for vert in vertices:
-        dl = vel_graph[vert][0][1]
+        dl =  vel_graph[vert][0][1] # fix if use depth: config.step *
         vel_graph[vert] += [dl / dt] # m/s
 
     return vel_graph
 
 
-def create_map(raw_lanes, pos, bboxes, kernel, old_bboxes):
+def test_func(nbb, obb, dt, depth_img):
+    ng = tracking(nbb, obb, depth_img)
+    if ng is not None:
+        vel_gr = calculate_velocity(dt, ng)
+        print('vel graph', vel_gr)
+
+
+def create_map(raw_lanes, bboxes, kernel, det_img, depth_img, dt=None, old_bboxes=None):
     homography = find_homography()
-    # raw_lanes - cv2.cvtColor(raw_lanes, cv2.COLOR_)
     ipm_map = ipm_ll(raw_lanes, homography)
+    det_ipm = ipm_ll(det_img, homography)
     # cv2.imwrite('ipm_lanes.jpg', ipm_map)
     lanes_map, peaks = lanes2map(ipm_map)
     # cv2.imshow('lanes_map', lanes_map)
-    steer = lane_centering(peaks, pos)
-    bird_eye_map = vehicles2map(bboxes, lanes_map)
-    obstacle_map = vehicles2map(bboxes, np.zeros_like(lanes_map))
+    steer = lane_centering(peaks)
+
+    if bboxes is not None:
+        # test_func(bboxes, old_bboxes, dt, depth_img)
+        bird_eye_map = vehicles2map(bboxes, lanes_map)
+        obstacle_map = vehicles2map(bboxes, np.zeros_like(lanes_map))
+        # print('source boxes', bboxes, old_bboxes)
+        # depth_img = cv2.medianBlur(depth_img, 17)
+        
+    else:
+        bird_eye_map = lanes_map
+        obstacle_map = np.zeros_like(lanes_map)
+
     extended_map = fast_convolution(obstacle_map, kernel)
 
-    return bird_eye_map, steer, extended_map
+    return bird_eye_map, steer, extended_map, lanes_map, det_ipm

lib/utils/plot.py
@@ -41,13 +41,13 @@ def show_seg_result(img, result, index, epoch, save_dir=None, is_ll=False,palett
         for label, color in enumerate(palette):
             color_seg[result == label, :] = color
     else:
-        color_area = np.zeros((result[0].shape[0], result[0].shape[1], 3), dtype=np.uint8)
+        color_area = np.zeros((result[1].shape[0], result[1].shape[1], 3), dtype=np.uint8)
         
         # for label, color in enumerate(palette):
         #     color_area[result[0] == label, :] = color
 
-        color_area[result[0] == 1] = [0, 255, 0]
-        color_area[result[1] ==1] = [255, 0, 0]
+        # color_area[result[0] == 1] = [0, 255, 0]
+        color_area[result[1] == 255] = [0, 255, 0]
         color_seg = color_area
 
     # convert to BGR
@@ -57,7 +57,7 @@ def show_seg_result(img, result, index, epoch, save_dir=None, is_ll=False,palett
     img[color_mask != 0] = img[color_mask != 0] * 0.5 + color_seg[color_mask != 0] * 0.5
     # img = img * 0.5 + color_seg * 0.5
     img = img.astype(np.uint8)
-    img = cv2.resize(img, (1280,720), interpolation=cv2.INTER_LINEAR)
+    # img = cv2.resize(img, (1280,720), interpolation=cv2.INTER_LINEAR)
 
     if not is_demo:
         if not is_gt:
@@ -82,6 +82,7 @@ def plot_subbox(x, img):
 
 def plot_one_box(x, img, color=None, label=None, line_thickness=None):
     # Plots one bounding box on image img
+    
     tl = line_thickness or round(0.0001 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
     color = color or (255,200,0)
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))

tools/detection.py
@@ -14,9 +14,7 @@ from PIL import Image
 
 from lib.core.general import non_max_suppression
 from lib.utils import plot_one_box, show_seg_result
-from lib.utils.plot import plot_subbox
 from lib.utils.mapping import *
-from lib.core.postprocess import morphological_process,  connect_lane
 
 
 def preprocess_image(image):
@@ -36,7 +34,7 @@ def detect(img, model):
     return det_out, da_seg_out, ll_seg_out
     
 
-def process(color_img,  det_out, da_seg_out, ll_seg_out, old_bboxes):# depth_scale # second pos was: depth_img,
+def postprocess(color_img,  det_out, ll_seg_out):#old_bboxes
         '''
         conf thres
         det_out[0:2]
@@ -49,17 +47,17 @@ def process(color_img,  det_out, da_seg_out, ll_seg_out, old_bboxes):# depth_sca
         # det = det_pred[0]
         inf_out, _ = det_out
         det_pred = non_max_suppression(inf_out, conf_thres=0.25, iou_thres=0.45, classes=None, agnostic=False)
-        det=det_pred[0]
+        det = det_pred[0]
 
         height, width, _ = color_img.shape
         pad_w, pad_h = 0, 0
         ratio = 1
 
-        da_predict = da_seg_out[:, :, pad_h:(height-pad_h),pad_w:(width-pad_w)]
-        da_seg_mask = torch.nn.functional.interpolate(da_predict, scale_factor=int(1/ratio), mode='bilinear')
-        _, da_seg_mask = torch.max(da_seg_mask, 1)
-        da_seg_mask = da_seg_mask.int().squeeze().cpu().numpy()
-        da_seg_mask = morphological_process(da_seg_mask, kernel_size=7)
+        # da_predict = da_seg_out[:, :, pad_h:(height-pad_h),pad_w:(width-pad_w)]
+        # da_seg_mask = torch.nn.functional.interpolate(da_predict, scale_factor=int(1/ratio), mode='bilinear')
+        # _, da_seg_mask = torch.max(da_seg_mask, 1)
+        # da_seg_mask = da_seg_mask.int().squeeze().cpu().numpy()
+        # da_seg_mask = morphological_process(da_seg_mask, kernel_size=7)
 
         '''
         pad_h =  0
@@ -72,56 +70,61 @@ def process(color_img,  det_out, da_seg_out, ll_seg_out, old_bboxes):# depth_sca
         _, ll_seg_mask = torch.max(ll_seg_mask, 1)
         ll_seg_mask = ll_seg_mask.int().squeeze().cpu().numpy()
 
-        # use raw data without shitty postprocess
-        
-        # ll_seg_mask = morphological_process(ll_seg_mask, kernel_size=7, func_type=cv2.MORPH_OPEN)
-        # ll_seg_mask = connect_lane(ll_seg_mask)
-        # ipm_img = ipm_ll(ll_seg_mask*255)
-       # cv2.imwrite('ipm_ll_seg.jpg', ipm_img)
-       # ll_map = lanes2map(ipm_img)
-        
-        pos = (640 // 2, 0)
-        xyxy = reversed(det)[:, :4]
-        xyxy = np.asanyarray(xyxy)
-        points = copy.copy(xyxy)
-        print('points: ', points)
-        points[:, 1] = points[:, 3] # NEED TO FIX 
-        points = points.reshape(-1, 2)
-        points = points.reshape(-1,1,2) #.astype(np.uint8) 
-        new_points = ipm_pts(points, find_homography())
-        # new_points = new_points.reshape(2, 2)
-        new_points = new_points.reshape(-1, 4)
-        new_points = np.array(new_points)
-        print('new points: ', new_points)
-       # print(new_points)
-        
-        ''''
-        check if new_points are on the map
-        transform button points to bird eye view, then vehicles2map
-        top points are useless
-        '''
+        # if len(det):
+        #     xyxy = reversed(det)[:, :4]
+        #     xyxy = np.asanyarray(xyxy)
+        #     points = copy.copy(xyxy)
+        #     # print('points: ', points)
+        #     points[:, 1] = points[:, 3]
+        #     points = points.reshape(-1, 2)
+        #     points = points.reshape(-1,1,2)
+        #     new_points = ipm_pts(points, find_homography())
+        #     # if new_points is not None:
+        #     new_points = new_points.reshape(-1, 4)
+        #     new_points = np.array(new_points)
+        #     # print('new points: ', new_points)
+        # else:
+        #      new_points = None
 
 
-        # print(ll_seg_mask.shape())
         ll_seg_mask = np.array(ll_seg_mask*255, dtype=np.uint8)
-        kernel = np.ones((int(config.l_jr // config.step), int(config.w_jr // config.step)))
-        bird_eye_map, steer, extended_map = create_map(ll_seg_mask, pos, new_points, kernel, old_bboxes) #xyxy # depth_scale
+        # kernel = np.ones((int(config.l_jr // config.step), int(config.w_jr // config.step)))
+        # bird_eye_map, steer, extended_map = create_map(ll_seg_mask, config.pos, new_points, kernel) # old_bboxes
 
 
-
-        img_det = show_seg_result(color_img, (da_seg_mask, ll_seg_mask), _, _, is_demo=True)
+        img_det = show_seg_result(color_img, (_, ll_seg_mask), _, _, is_demo=True)
         img_det = cv2.resize(img_det, dsize=(640, 480))
-        # depth_img = cv2.resize(depth_img, dsize=(640, 480))
+
+        new_points_arr = []
 
         if len(det):
-           # det[:,:4] = scale_coords(color_img.shape[2:],det[:,:4],img_det.shape).round()
-            for *xyxy,conf,cls in reversed(det):
-                plot_one_box(xyxy, img_det , line_thickness=2)
-                # print(xyxy)
-               # plot_subbox(xyxy, img_det)
+            
+            for *xyxy, conf, _ in reversed(det):
+                # print('conf', float(conf.numpy()), type(float(conf.numpy())))
+                if float(conf.numpy()) >= 0.6:
+                    plot_one_box(xyxy, img_det , line_thickness=2)
+
+                    xyxy = np.asanyarray(xyxy)
+                    print(xyxy)
+                    points = copy.copy(xyxy)
+                    # print('points: ', points)
+                    points[1] = points[3]
+                    points = points.reshape(-1, 2)
+                    points = points.reshape(-1, 1, 2)
+                    new_points = ipm_pts(points, find_homography())
+                    print('_new', new_points)
+                    new_points = new_points.reshape(-1, 4)
+                    new_points = np.array(new_points)
+                    new_points_arr.append(new_points)
+                    print('new', new_points)
+                else: 
+                    continue
+
+            new_points_arr = np.vstack(new_points_arr)
+        else:
+            new_points_arr = None
+
+        
         
        
-        return img_det, bird_eye_map, steer, extended_map, new_points
-      #  cv2.imshow('image', img_det)
-       # cv2.imshow('map', ll_map)
-       
+        return img_det, new_points_arr, ll_seg_mask    

tools/main.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 import torch
 import time
-from detection import detect, process
+from detection import detect, postprocess
 import argparse
 from lib.config import cfg
 from lib.utils.utils import create_logger, select_device
@@ -44,8 +44,20 @@ def rs_stream(model):
     # align depth to rgb
     align = rs.align(rs.stream.color)
     depth_scale = profile.get_device().first_depth_sensor().get_depth_scale()
-    print('depth scale: ', depth_scale)
     old_bboxes = None
+    kernel = np.ones((int(config.l_jr // config.step), int(config.w_jr // config.step)))
+    # t1 = None
+
+    frames = pipe.wait_for_frames()
+    time.sleep(5)
+    aligned_frames = align.process(frames)
+    color_frame = aligned_frames.get_color_frame()
+    t0 = time.time()
+    color_image = np.asanyarray(color_frame.get_data())
+    # cv2.imwrite('test_lab.jpg', color_image)
+    det_out, _, ll_seg_out = detect(color_image, model)
+    _, old_bboxes, _ = postprocess(color_image, det_out, ll_seg_out)
+
 
     while True:
         start_time = time.time()
@@ -55,36 +67,38 @@ def rs_stream(model):
         depth_frame = aligned_frames.get_depth_frame()
         color_frame = aligned_frames.get_color_frame()
 
-        # depth_frame = frame.get_depth_frame()
-        # color_frame = frame.get_color_frame()
-
-        if not color_frame or not depth_frame: # ???
+        if not color_frame or not depth_frame:
             continue
 
         depth_image = np.asanyarray(depth_frame.get_data())
         color_image = np.asanyarray(color_frame.get_data())
+        
 
         depth_cm = cv2.applyColorMap(cv2.convertScaleAbs(depth_image,
                                         alpha = 0.5), cv2.COLORMAP_JET)
-        
-        det_out, da_seg_out, ll_seg_out = detect(color_image, model)
-        det_img, bird_eye_map, steer, old_bboxes = process(color_image, det_out, da_seg_out, ll_seg_out, old_bboxes)
-        
-        # lane centering
+
+        det_out, _, ll_seg_out = detect(color_image, model)
+        det_img, new_bboxes, ll_seg_mask = postprocess(color_image, det_out, ll_seg_out)
+        dt = time.time() - t0 #if t1 != None else None
+        t0 = time.time()
+        bird_eye_map, steer, extended_map, l_map, det_ipm = create_map(ll_seg_mask, new_bboxes, kernel, det_img, depth_image, dt, old_bboxes)
         # steering(steer, car)
         # add PID control
-        # add minkovski sum and path planning
+        # path planning
 
-        
-
-        cv2.imshow('rgb', color_image)
-       # cv2.imshow('rgb', ipm_img)
-        cv2.imshow('depth', depth_cm)
+        #cv2.imshow('rgb', color_image)
+        # cv2.imshow('rgb', ipm_img)
+        cv2.imshow('source', det_img)
+        cv2.imshow('bev', bird_eye_map)
+       
+        # cv2.imshow('detected', det_ipm)
+        cv2.imshow('extended', extended_map)
 
         if cv2.waitKey(1) == ord('q'):
             break
 
         end_time = time.time()
+        old_bboxes = new_bboxes
 
     pipe.stop()
     cv2.destroyAllWindows() 
@@ -93,15 +107,18 @@ def put_img(model, frame):
     frame = np.asanyarray(frame)
     frame = cv2.resize(frame, dsize=(640, 480))
     frame = np.asanyarray(frame)
+    kernel = np.ones((int(config.l_jr // config.step), int(config.w_jr // config.step)))
 
     det_out, da_seg_out, ll_seg_out = detect(frame, model)
-    det_img, bird_eye_map, steer, extended_map = process(frame, det_out, da_seg_out, ll_seg_out)
+    # det_img, bird_eye_map, steer, extended_map = postprocess(frame, det_out, da_seg_out, ll_seg_out)
+    det_img, new_bboxes, ll_seg_mask = postprocess(frame, det_out, ll_seg_out, old_bboxes)
+    bird_eye_map, steer, extended_map = create_map(ll_seg_mask, new_bboxes, kernel, old_bboxes)
     #det_img = Image.fromarray(det_img)
     # cv2.imwrite('test_ll.jpg', ll_seg_mask*255)
     # ll = ll_seg_mask*255
     # ll = cv2.cvtColor(ll, cv2.COLOR_GRAY2BGR)
     # det_img_1 = cv2.cvtColor(det_img, cv2.COLOR_RGB2BGR)
-    steering(steer)
+    # steering(steer)
     
     while True:
         cv2.imshow('detection', det_img)
@@ -116,6 +133,7 @@ def put_img(model, frame):
 def cv_stream(model):
 
     vid = cv2.VideoCapture(0) 
+    kernel = np.ones((int(config.l_jr // config.step), int(config.w_jr // config.step)))
     
     while(True): 
         
@@ -125,8 +143,10 @@ def cv_stream(model):
         # cv2.imwrite('cv_frame.jpg', frame)
 
         det_out, da_seg_out, ll_seg_out = detect(frame, model)
-        det_img, bird_eye_map, steer = process(frame, det_out, da_seg_out, ll_seg_out)
-        steering(steer)
+        # det_img, bird_eye_map, steer = postprocess(frame, det_out, da_seg_out, ll_seg_out)
+        det_img, new_bboxes, ll_seg_mask = postprocess(frame, det_out, ll_seg_out, old_bboxes)
+        bird_eye_map, steer, extended_map = create_map(ll_seg_mask, new_bboxes, kernel, old_bboxes)
+        # steering(steer)
         
         
         cv2.imshow('rgb', det_img) 
@@ -138,13 +158,6 @@ def cv_stream(model):
     
     vid.release() 
     cv2.destroyAllWindows() 
-
-# if __name__ == '__main__':
-#     # load model
-#     model = torch.hub.load('hustvl/yolop', 'yolop', pretrained=True)
-#     #  model.eval()
-#    # rs_stream(model)
-#     
     
 
 if __name__ == '__main__':
@@ -161,8 +174,8 @@ if __name__ == '__main__':
     opt = parser.parse_args()
     with torch.no_grad():
         model = load_model()
-        # rs_stream(model)
+        rs_stream(model)
         # cv_stream(model)
-        img = Image.open('rs_color_img2.jpg').convert("RGB")  # rs_color_img2.jpg
-        put_img(model, img)
+        # img = Image.open('rs_color_img2.jpg').convert("RGB")  # rs_color_img2.jpg
+        # put_img(model, img)
         cv2.destroyAllWindows()