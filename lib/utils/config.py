5115354d7c031e1906950977e417d1178cdb1b91
legonko <suslova02@mail.ru>
2024-04-29 17:59:30 +0300

    add merge for graph

--- lib/utils/config.py
+++ lib/utils/config.py
@@ -1,8 +1,10 @@
 w_jr = 0.15 # 0.03
 l_jr = 0.25 # 0.075
 depth_scale = 0.0010000000474974513
+# add the second depth scale
 step = 0.00017578125
 k_pm = 1672.7272727272725 # pixels/meters
 pos = (1040 // 2, 0)
 row_add = 400
 column_add = 400
+l, w = 640, 480
